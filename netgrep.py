302082132957b65942622f16c1d9e39e196db6a2
Phillip Stansell <pstansel@roland.darktower.stansells.org>
2020-11-27 20:11:17 -0700

    Handle IPv4 network masks and wildcard (host) masks seperated from a network or interface address by a space. Optimize the network search list.

--- netgrep.py
+++ netgrep.py
@@ -4,7 +4,7 @@
 within the networks.
 """
 __author__ = "Phillip Stansell"
-__version__ = "0.9"
+__version__ = "0.91"
 
 import argparse
 import ipaddress
@@ -58,27 +58,90 @@ def build_output_line(parts: dict, colorize: bool) -> str:
 
     return result
 
-def search_networks(tokens: list, networks: list) -> list:
+def clean_up_networks(strings: list) -> tuple:
     """
-    Take a list of strings and a list of IPv[46]Network objects and return a
-    list of indexes from the first list that were found within a network from
-    the second list.
+    Take a list of strings. Convert elements to objects from the ipaddress
+    class. For elements that fail conversion (still string), print an error and
+    remove them. Separate remaining items into lists of a single address family:
+    one of IPv4 objects and the other of IPv6 objects. Convert Interface objects
+    to Network objects before inserting into the appropriate address family
+    list. Return a tuple of the IPv4Network list, IPv6Network list.
+    """
+    ip4_nets = []
+    ip6_nets = []
+    # Convert strings to ipaddress objects
+    ip_networks = strings_to_networks(strings)
+
+    # Print an error for each string that did not convert and remove it from
+    # the list. Sort IPv4 and IPv6 objects into separate lists while converting
+    # any Interface objects to Network objects.
+    for i,item in enumerate(ip_networks):
+        if isinstance(item, str):
+            print("ValueError: '{}' does not appear to be an IPv4 or IPv6 "
+                    "network, ignoring.".format(item))
+            del ip_networks[i]
+        elif isinstance(item, ipaddress.IPv4Interface):
+            ip4_nets.append(item.network)
+        elif isinstance(item, ipaddress.IPv4Network):
+            ip4_nets.append(item)
+        elif isinstance(item, ipaddress.IPv6Interface):
+            ip6_nets.append(item.network)
+        elif isinstance(item, ipaddress.IPv6Network):
+            ip6_nets.append(item)
+        else:
+            raise TypeError("Unexpected type for item {} found at "
+            "'ip_networks[{}]'.".format(str(item), i))
+
+    # Return collapsed lists
+    return (list(ipaddress.collapse_addresses(ip4_nets)),
+            list(ipaddress.collapse_addresses(ip6_nets)))
+
+def search_networks(tokens: list, ipv4_networks: list, ipv6_networks: list) -> list:
+    """
+    Take a list of strings, a list of IPv4Network objects, and a list of
+    IPv6Network objects. Return a list of indexes from the first list that were
+    found within a network from the second or third list.
     """
     result = []
     # Convert tokens to IPv[46]Network or IPv[46]Interface objects
     ip_tokens = strings_to_networks(tokens)
 
-    # If any networks are found in the line, print it
+    # If any token has converted to an ipaddress object, check if it is a
+    # subnet of any member of the matching address family list. For sucessful
+    # subnet matches, add the ip_token list index to the result list.
     for i, token in enumerate(ip_tokens):
+        # Convert any Interface objects to Network Objects
         if isinstance(token, (ipaddress.IPv4Interface, ipaddress.IPv6Interface)):
             token = token.network
+
         if isinstance(token, ipaddress.IPv4Network):
-            for net in [ net4 for net4 in networks if isinstance(net4, ipaddress.IPv4Network) ]:
+            # For host length networks, look ahead to the next token. If is a
+            # subnet mask or host mask, replace both tokens with the corrected
+            # network.
+            if token.prefixlen == 32 and (i + 1) < len(ip_tokens):
+                j = i + 1
+                next_token = ip_tokens[j]
+                if isinstance(next_token, ipaddress.IPv4Network) and next_token.prefixlen == 32:
+                    new_token = strings_to_networks([ str(token.network_address)
+                        + "/"
+                        + str(next_token.network_address) ])[0]
+                    if isinstance(new_token, ipaddress.IPv4Interface):
+                        new_token = new_token.network
+                    if isinstance(new_token, ipaddress.IPv4Network):
+                        token = new_token
+                        ip_tokens[j] = new_token
+
+            # Check if IPv4Network object is subnet of any ipv4_networks list
+            # member.
+            for net in ipv4_networks:
                 if token.subnet_of(net):
+                    # print("Token {} matched network {}.".format(token, net))
                     result.append(i)
                     break
         elif isinstance(token, ipaddress.IPv6Network):
-            for net in [ net6 for net6 in networks if isinstance(net6, ipaddress.IPv6Network) ]:
+            # Check if IPv6Network object is subnet of any ipv6_networks list
+            # member.
+            for net in ipv6_networks:
                 if token.subnet_of(net):
                     result.append(i)
                     break
@@ -89,7 +152,7 @@ def strings_to_networks(strings: list) -> list:
     """
     Take a list of strings and return a list with elements converted to objects
     of type IPv4Network or IPv6Network where possible. Strings that could not be
-    converted are copied.
+    converted are copied unchanged.
     """
     result = []
 
@@ -133,17 +196,9 @@ def main():
         print("NOTICE: For color support, install the 'colorama' python module.")
 
     # Convert network strings to ipaddress objects
-    ip_networks = strings_to_networks(args.networks)
-    # Print an error for each network string that did not convert and remove
-    # it from the list. Convert any IPv[46]Interface objects to IPv[46]Network
-    # objects.
-    for i,item in enumerate(ip_networks):
-        if isinstance(item, str):
-            print("ValueError: '{}' does not appear to be an IPv4 or IPv6 "
-                    "network, ignoring.".format(item))
-            del ip_networks[i]
-        if isinstance(item, (ipaddress.IPv4Interface, ipaddress.IPv6Interface)):
-            ip_networks[i] = item.network
+    ipv4_networks, ipv6_networks = clean_up_networks(args.networks)
+    # print("ipv4_networks after conversion and collapsing: {}".format(ipv4_networks))
+    # print("ipv6_networks after conversion and collapsing: {}".format(ipv6_networks))
 
     # Flatten list of target files
     targets = [ y for x in args.target_files for y in x ]
@@ -172,7 +227,7 @@ def main():
                 line_tokens = line.strip("\n").split(" ")
                 # Get indexes from line_tokens that are within an element of
                 # ip_networks
-                matches = search_networks(line_tokens, ip_networks)
+                matches = search_networks(line_tokens, ipv4_networks, ipv6_networks)
                 # If we found a match, print the line
                 if len(matches) > 0:
                     print(build_output_line({"file_name": target,
